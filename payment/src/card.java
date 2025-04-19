74067de8e6debd56162ef0632f810248ec5e4d5a
MokshSharma2003 <mokshsharma13@gmail.com>
2025-04-19 19:30:06 +0530

    third comit

---
+++ payment/src/card.java
@@ -0,0 +1,8 @@
+public abstract class card implements ipayment{
+    protected int cvv;
+    protected String cardnumber;
+    protected String name;
+
+
+
+}
---
+++ payment/src/ipayment.java
@@ -0,0 +1,3 @@
+public interface ipayment {
+    void pay();
+}
